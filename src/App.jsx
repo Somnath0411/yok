40b3b166cff40ef51b31f5632e4d18590d5b3b4d
Harrish-30 <harrishhm30@gmail.com>
2025-12-30 12:32:00 +0530

    corrected routing

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/services" element={<Servicespage/>}/>
-            <Route element={<NotFound/>}/>
+            <Route path="*" element={<NotFound/>}/>
           </Routes>
         <Footer />
       </div>
