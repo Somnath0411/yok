6106413a49dce5369f3e05febef1fe44c307445c
Harrish-30 <harrishhm30@gmail.com>
2026-01-19 14:05:05 +0530

    fixed major responsiveness issues

--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -5,9 +5,24 @@ export default function WhyChoose(){
   return (
     <div className="symbots-difference-bg">
       <section className="why">
-      <div className="why-inner">
+      <div className="why-inner lg-screen">
         <div className="why-text ">
-            <h2 className="txt-48">The <span className="prim-blue">Symbotz</span> Difference</h2>
+          <h2 className="txt-48">The <span className="prim-blue">Symbotz</span> Difference</h2>
+            <p className="muted darkblue-text txt-19">Connect your goals to the systems that support them.
+            Build with clarity, scale with cloud confidence, and move faster with data guiding the way.</p>
+            <ul className="expertise darkblue-text txt-19">
+              <li className="active"><span className="dot active"/> Built right from the start</li>
+              <li className="active"><span className="dot active"/> Scales when you need it</li>
+              <li className="active"><span className="dot active"/> Support that stays with you</li>
+            </ul>
+        </div>
+          <div className="why-media">
+            <img src="https://res.cloudinary.com/dra2qc1c7/image/upload/v1767875270/pexels-fauxels-3184405_oatkbb.jpg" alt="illustration" />
+          </div>
+      </div>
+      <div className="why-inner sm-screen">
+        <h2 className="txt-48">The <span className="prim-blue">Symbotz</span> Difference</h2>
+        <div className="why-text ">
             <p className="muted darkblue-text txt-19">Connect your goals to the systems that support them.
             Build with clarity, scale with cloud confidence, and move faster with data guiding the way.</p>
             <ul className="expertise darkblue-text txt-19">
