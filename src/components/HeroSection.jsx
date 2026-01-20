1a1286ab120c02d51b483b1ea434465dc6912dd0
Somnath0411 <sbsomnath007@gmail.com>
2026-01-20 18:45:57 +0530

    animation changes trying out

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,14 +3,14 @@ const HeroSection = (props) =>{
     return(
         <section className="hero-section">
         <div className="hero-content">
-          <h1 className="main-heading">{title}</h1>
+          <h1 className="main-heading fade-up">{title}</h1>
           <p className="hero-description txt-19">
             {description}
           </p>
-          <button className="btn-primary">{btncontent}</button>
+          <button className="btn-primary fade-up">{btncontent}</button>
         </div>
         <div className="hero-image-placeholder">
-          <img className="hero-image" src={imgsrc} alt='yokesh' />
+          <img className="hero-image fade-up" src={imgsrc} alt='yokesh' />
         </div>
       </section>
     )
--- src/components/Learn/LearnPage.jsx
+++ src/components/Learn/LearnPage.jsx
@@ -6,75 +6,75 @@ const LearnPage= () => {
   const courses=[{
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
     name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
   name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
   name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
   name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
   name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
   },
   {
    name:"python",
    description:"learn python in an futuristic way",
-   duration:"48 hrs",
+   duration:"🕛 48 hrs",
    img:"https://res.cloudinary.com/dra2qc1c7/image/upload/v1768029525/xufhduongclasyxd9c2u.jpg"
-  },
+  }
 ]
     return (
       <section className="learn-section">
